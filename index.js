1ba33f2e832e8bdcc247f7164274f534a23c4401
Tarciso <tarcisoloiola@hotmail.com>
2017-08-08 18:54:05 -0400

    done

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function myFunction_about(){
 	document.geElementsByTagName("section").style.display="none";
 }
+
+
+function myFunction_portfolio(){
+	document.geElementsByTagName("section").style.display="none";
+}
+
+function myFunction_contact(){
+	document.geElementsByTagName("section").style.display="none";
+}
